0d787d231651ba78cec2a84ff26c19e96e401361
Alex Clough <alexcl123@gmail.com>
2021-04-23 14:22:29 -0400

    before lunch

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import * as sessionActions from '../../store/session'
+
 const ProfileButton = ({user}) => {
+    const dispatch = useDispatch()
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
       <div>
-        <button>
+        <button onClick={setShowMenu(true)}>
           <i className="fas fa-record-vinyl"></i>
         </button>
+        {showMenu && (
+            <ul>
+                <li>{user.name}</li>
+                <li>{user.email}</li>
+                <button onClick={dispatch(sessionActions.logout)}></button>
+            </ul>
+        )}
+
       </div>
     );
 }
