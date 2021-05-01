6ff5e1d16f64e812899b508499e7d78f646a4233
Alex Clough <alexcl123@gmail.com>
2021-05-01 13:16:55 -0400

    before reseeding db

--- backend/data/userCollection.js
+++ backend/data/userCollection.js
@@ -1,13 +1,25 @@
-let firstGroup = []
-let secondGroup = []
-let thirdGroup = []
-let fourthGroup = []
-for (let i =1; i < 20; i++) {
-let collectionObj = {}
-collectionObj.userId = i
-collectionObj.albumId = 1
-collectionObj.comment = 'great job!'
-firstGroup.push(collectionObj)
+const faker = require("faker");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let comments = []
+for (let i=0; i < 40, i ++) {
+  const comment = fake.lorem.sentence()
+  comments.push(comment)
+}
+
+let firstGroup = [];
+let secondGroup = [];
+let thirdGroup = [];
+let fourthGroup = [];
+for (let i = 1; i < 20; i++) {
+  let collectionObj = {};
+  collectionObj.userId = i;
+  collectionObj.albumId = 1;
+  collectionObj.comment = comments[getRandomInt(comments.length)]
+  firstGroup.push(collectionObj);
 }
 
 for (let i = 1; i < 40; i++) {
@@ -24,17 +36,14 @@ for (let i = 10; i < 30; i++) {
   thirdGroup.push(collectionObj);
 }
 
-for (let i = 10; i < 30; i=i+2) {
+for (let i = 10; i < 30; i = i + 2) {
   let collectionObj = {};
   collectionObj.userId = i;
   collectionObj.albumId = 4;
   fourthGroup.push(collectionObj);
 }
 
-exportGroup = [...firstGroup, ...secondGroup, ...thirdGroup]
-console.log(exportGroup)
+exportGroup = [...firstGroup, ...secondGroup, ...thirdGroup];
+console.log(exportGroup);
 
-
-
-
-module.exports = exportGroup
+module.exports = exportGroup;
