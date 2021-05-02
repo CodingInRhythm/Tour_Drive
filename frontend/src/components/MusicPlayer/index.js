2187562ff4854dd49bf7c5ffb44dcba2525a232f
Alex Clough <alexcl123@gmail.com>
2021-05-02 17:08:04 -0400

    activated song Links

--- frontend/src/components/MusicPlayer/index.js
+++ frontend/src/components/MusicPlayer/index.js
@@ -1,69 +1,72 @@
 import AudioControls from '../AudioControls'
 import { useState, useRef, useEffect } from 'react'
 const MusicPlayer = ({tracks}) => {
-    //codinginrhythmbucket.s3.amazonaws.com/03+-+Red+Shades.wav
-    const [ trackIndex, setTrackIndex ] = useState(0)
-    // const [ trackProgress, setTrackProgress] = useState(0)
-    const [ isPlaying, setIsPlaying ] = useState(false)
-   
+  //codinginrhythmbucket.s3.amazonaws.com/03+-+Red+Shades.wav
+  const [trackIndex, setTrackIndex] = useState(0);
+  // const [ trackProgress, setTrackProgress] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    const { name, songLink } = tracks[trackIndex];
-    
-    const audioRef = useRef(new Audio());
-    // const audioRef = useRef(new Audio(songLink));
-    // const intervalRef = useRef();
-    // const isReady = useRef(false);
-    
-   //trackIndex use Effect
+  const { name, songLink } = tracks[trackIndex];
+
+  const audioRef = useRef(new Audio(songLink));
+  // const audioRef = useRef(new Audio(songLink));
+  // const intervalRef = useRef();
+  // const isReady = useRef(false);
+
+  //trackIndex use Effect
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      audioRef.current = new Audio(tracks[trackIndex].songLink);
+      // audioRef.current = new Audio(tracks[trackIndex].songLink);
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+    // startTimer()
+  }, [trackIndex]);
+
+  useEffect(() => {
+    if (isPlaying === false) {
+      audioRef.current.pause();
+      // startTimer()
+    } else {
+      audioRef.current.play();
+    }
+  }, [isPlaying]);
+
     useEffect(() => {
-   
-        if (isPlaying) {
-        audioRef.current.pause()
-        audioRef.current = new Audio()
-        // audioRef.current = new Audio(tracks[trackIndex].songLink);
-        audioRef.current.play()
-        setIsPlaying(true)
-        }
-        // startTimer()
-      }, [trackIndex]);
-
-
-    useEffect(() => {
- 
-      if (isPlaying === false) {
+      console.log('useEffect 2')
+      // Pause and clean up on unmount
+      return () => {
         audioRef.current.pause();
-        // startTimer()
-      } else {
-        audioRef.current.play()
-      }
-    }, [isPlaying]);
-    
-    return (
-      <>
-        {tracks.map((song, idx) => (
-          <div>
-            
-            <AudioControls
-              song={song}
-              idx={idx}
-              trackIndex={trackIndex}
-              setTrackIndex={setTrackIndex}
-              setIsPlaying={setIsPlaying}
-              isPlaying={isPlaying}
-              // trackProgress={trackProgress}
-              // duration={duration}
-              // onScrub={onScrub}
-              // onScrubEnd={onScrubEnd}
-            />
-         
-          </div>
-        ))}
-        {/* <audio controls>
+        // clearInterval(intervalRef.current);
+      };
+    }, []);
+
+  return (
+    <>
+      {tracks.map((song, idx) => (
+        <div>
+          <AudioControls
+            song={song}
+            idx={idx}
+            trackIndex={trackIndex}
+            setTrackIndex={setTrackIndex}
+            setIsPlaying={setIsPlaying}
+            isPlaying={isPlaying}
+            // trackProgress={trackProgress}
+            // duration={duration}
+            // onScrub={onScrub}
+            // onScrubEnd={onScrubEnd}
+          />
+        </div>
+      ))}
+      {/* <audio controls>
         <source src={songUrl}/>
       </audio> */}
-      </>
-    );
-    }
+    </>
+  );
+}
     //functions
 
     //this function is for a slider which I'm not implementing as MVP
@@ -126,15 +129,8 @@ const MusicPlayer = ({tracks}) => {
   //trackIndex useEffect
 
 
+
   // //cleans up 
-  //   useEffect(() => {
-  //     console.log('useEffect 2')
-  //     // Pause and clean up on unmount
-  //     return () => {
-  //       audioRef.current.pause();
-  //       // clearInterval(intervalRef.current);
-  //     };
-  //   }, []);
     // const { duration } = audioRef.current;
 
 export default MusicPlayer
